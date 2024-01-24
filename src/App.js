cd2985e0a84b75edb4b33ce9067c347825e1f45a
Jigin Jayaprakash <145117767+JiginJayaprakash@users.noreply.github.com>
2024-01-24 09:00:31 +0000

    updating files and build

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Octokit } from '@octokit/core'
 
 function App() {
+  let _repoName = '';
   const githubAccessToken = ''
   const octokit = new Octokit({ auth: githubAccessToken });
   document.addEventListener('DOMContentLoaded', function () {
@@ -17,7 +18,8 @@ function App() {
 
   const createGitHubRepo = async (repoName, repoDescription) => {
     try {
-      await octokit.request('POST /user/repos', {
+      _repoName = repoName
+      let result = await octokit.request('POST /user/repos', {
         name: repoName,
         description: repoDescription,
         homepage: 'https://github.com',
@@ -26,28 +28,66 @@ function App() {
         headers: {
           'X-GitHub-Api-Version': '2022-11-28'
         }
-      })
+      });
+      console.log(result);
     } catch (error) {
       console.error('Error creating repository:', error.message);
     }
   }
 
+  const pushFilestoRepo = async () => {
+    let result = await octokit.request('PUT /repos/JiginJayaprakash/testingStuff/contents/text.html', {
+      owner: 'JiginJayaprakash',
+      repo: 'testingStuff',
+      path: 'index.html',
+      message: 'my commit message',
+      content: 'bXkgbmV3IGZpbGUgY29udGVudHM=',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(result);
+  }
 
+  const createGithubPage = async() => {
+    await octokit.request('POST /repos/JiginJayaprakash/testingStuff/pages', {
+      owner: 'JiginJayaprakash',
+      repo: 'testingStuff',
+      source: {
+        branch: 'main',
+        path: '/root'
+      },
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+  }
+  const triggerGithubBuild = async() => {
+   let result = await octokit.request('POST /repos/JiginJayaprakash/testingStuff/pages/builds', {
+      owner: 'JiginJayaprakash',
+      repo: 'testingStuff',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+  }
+
+  const getLatestBuild = async() => {
+   const result =  await octokit.request('GET /repos/{owner}/{repo}/pages/builds/latest', {
+      owner: 'OWNER',
+      repo: 'REPO',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    return result;
+  }
 
   const test = async () => {
     await createGitHubRepo('testingStuff', 'test successful');
-    const articleFiles = [
-      {
-          path: "/index.html",
-          content: "Hello World!",
-          encoding: "utf-8",
-      },
-      {
-          path: "/images/logo.png",
-          content: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQ",
-          encoding: "base64",
-      },
-  ];
+    await pushFilestoRepo();
+    await createGithubPage();
   }
   return (
     <div className="App">
